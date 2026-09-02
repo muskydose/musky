18bd9afe124405697ef4e5d62731f9111d2cf16c
MUSKY DOSE <muskydose@gmail.com>
2026-09-02 23:46:23 +0530

    feat(ui): mount global page transition

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -10,12 +10,14 @@ import SearchDrawer from '@/components/SearchDrawer';
 import CategoryDrawer from '@/components/CategoryDrawer';
 import AccountDrawer from '@/components/AccountDrawer';
 import NotificationsDrawer from '@/components/NotificationsDrawer';
+import GlobalPageTransition from '@/components/GlobalPageTransition';
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <UIProvider>
       <CartProvider>
         <WishlistProvider>
+          <GlobalPageTransition />
           {children}
           <CartDrawer />
           <WishlistDrawer />
